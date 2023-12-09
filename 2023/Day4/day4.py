bb3668d75b0846abac445a1511ad775bb0689ec6
JoshBenn <jdbenn@asu.edu>
2023-12-09 13:33:00 -0700

    fixed day4 code, apparently messed it up before pushing

--- 2023/Day4/day4.py
+++ 2023/Day4/day4.py
@@ -2,37 +2,32 @@ with open("data.txt", "r") as file:
     lines = file.readlines()
 
 class Card:
+    __slots__ = ['count']
     def __init__(self, count):
         self.count = count
-
 
 cards = []
 points = 0
 copies = 0
 for line in lines:
     line = line.replace('\n', '')
-    (cardnum, numbers) = line.split(':')
-    (win_nums, my_nums) = numbers.split('|')
-    win_nums = win_nums.split()
-    my_nums = my_nums.split()
+    cardnum, numbers = line.split(':')
+    win_nums, my_nums = numbers.split('|')
+    win_nums = set(win_nums.split())
+    my_nums = set(my_nums.split())
 
-    count = sum(1 for num in my_nums if num in win_nums)
+    copies += 1
+    count = len(win_nums.intersection(my_nums))
 
     for card in cards[:]:
         copies += 1
-        card.count -=1
+        card.count -= 1
         cards.append(Card(count))
-    cards = [card for card in cards if card.count > 0]
-            
-    copies += 1
     cards.append(Card(count))
+    cards = [card for card in cards if card.count > 0]
 
     if count > 0:
         points += 2**(count - 1)
-
-
-for counter in cards:
-    copies += counter.count
 
 print("Part 1: " + str(points))
 print("Part 2: " + str(copies))
